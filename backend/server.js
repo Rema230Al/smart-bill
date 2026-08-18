d25b18b0ff1f8a244c236aec18b5386cb3f835e7
R_ema1 <remasalsulami962@gmail.com>
2026-08-18 06:54:13 +0300

    path

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const {parseReceiptText}= require('./services/receiptService');
 
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'زز')));
+app.use(express.static(path.join(__dirname, '..')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..','index.html'));
